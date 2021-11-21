9104e9d1f58357515ea6549705b59b10dc13ea03
André Schreck <moin@andreschreck.de>
2021-11-21 23:23:28 +0100

    fixed imports

--- src/test/java/de/neuefische/java/week2/shopping/ShopServiceTest.java
+++ src/test/java/de/neuefische/java/week2/shopping/ShopServiceTest.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ShopServiceTest {
 
